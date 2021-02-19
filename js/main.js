f2081e37c31bcba154e18a7c31d12bf382d2e5b3
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-02-19 16:07:19 -0500

    First push

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//interval is 1000 milliseconds (1 second). Function executes every second.
+//25 minutes in seconds is 1500 seconds
+
+let project = setInterval(countdownTimer, 1000);
+
+// Total number of minutes in seconds. 25 * 60. Global variable aka outside of the function. 
+let total = 1500;
+
+// remember -- % is remainder of x and y, while / is quotient of x and y
+function countdownTimer() {
+//Referencing html to display the timer
+    let display = document.getElementById("RachelsTimer")
+    //originally I used parseInt for both to ensure they were #s not strings, but Math.floor allows me to just take the minutes and leave the remainder to the seconds.
+    let minutes = Math.floor(total / 60);
+    let seconds = (Math.floor((total%60) % 60)).toString().padStart(2,'0');
+    if (total >= 0) {
+        display.innerHTML = minutes + ":" + seconds;
+        total = total - 1;
+    }
+    else if (total == 0) {
+         clearInterval(countdownTimer);
+         //return is included to indicate that the function is ended.
+         return;
+    }
+}
+
+
