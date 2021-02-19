50a4eaf1ed277a86300e3deb9ccd0e8de016cbb1
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-02-19 17:03:21 -0500

    first audio attempt

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@
 let project = setInterval(countdownTimer, 1000);
 
 // Total number of minutes in seconds. 25 * 60. Global variable aka outside of the function. 
-let total = 1500;
+let total = 10;
 
 // remember -- % is remainder of x and y, while / is quotient of x and y
 function countdownTimer() {
 //Referencing html to display the timer
     let display = document.getElementById("RachelsTimer")
+    let audio = new Audio("/Users/rachelehlers/BootCamp/CountdownTimer/audio/TomatoSong.mp3");
     //originally I used parseInt for both to ensure they were #s not strings, but Math.floor allows me to just take the minutes and leave the remainder to the seconds.
     let minutes = Math.floor(total / 60);
     let seconds = (Math.floor((total%60) % 60)).toString().padStart(2,'0');
@@ -19,9 +20,13 @@ function countdownTimer() {
     }
     else if (total == 0) {
          clearInterval(countdownTimer);
+         audio.play();
          //return is included to indicate that the function is ended.
          return;
     }
 }
 
-
+//I used this to try to get the button to load last, but it hasn't worked as of yet.
+//var btn = document.createElement("Button");
+//btn.innerHTML = "What's happening?";
+//document.body.appendChild(btn);
